615628cb3b0820f6f65951b1be2772b9e014fcec
Debasish Bhola <devop69@gmail.com>
2024-06-25 10:37:19 +0530

    Completed Routing

--- 6Routing/src/App.jsx
+++ 6Routing/src/App.jsx
@@ -1,14 +1,40 @@
+import { Suspense,lazy } from "react";
+import { BrowserRouter, Routes, Route,useNavigate } from "react-router-dom";
+
+const Dashboard = lazy(()=>import('./components/Dashboard'))
+const Landing = lazy(()=>import('./components/Landing'))
 
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+  function App() {
+    return (
+    //Using suspense API--->Asynchronous component fetch,data fetch
+    <BrowserRouter>
+    <AppBar></AppBar>
+      <Routes>
+        <Route path="/dashboard" element={<Suspense fallback={'Loading...'}><Dashboard/></Suspense>}></Route>
+        <Route path="/" element={<Suspense fallback={'Loading...'}><Landing/></Suspense>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+function AppBar() {
+  const navigate = useNavigate();
   return (
     <>
-     
+      <div>
+        <div>
+          <button onClick={()=>{
+            //window.location.href='/';
+            navigate("/");
+          }}>Landing Page</button>
+          <button onClick={()=>{
+            //window.location.href='/dashboard';
+            navigate("/dashboard")
+          }}>Dashboard Page</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
