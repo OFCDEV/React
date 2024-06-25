8fdadc0d3dceb4f9e1a16ebd372adc2177e9ce3d
Debasish Bhola <devop69@gmail.com>
2024-06-25 10:50:53 +0530

    Counter Created

--- 7PropDrilling/src/App.jsx
+++ 7PropDrilling/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Prop Drilling --->Getting props from Least Common Ancestor(Imagine as a binary tree)
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Count count={count} />
+      <Button count={count} setCount={setCount} />
     </>
-  )
+  );  
+}
+function Count({ count }) {
+  return <div>{count}</div>;
 }
 
-export default App
+function Button({count,setCount}) {
+  return (
+    <div>
+      <button onClick={() => {setCount(count+1)}}>Increase</button>
+
+      <button onClick={() => {setCount(count-1)}}>Decrease</button>
+    </div>
+  );
+}
+
+export default App;
