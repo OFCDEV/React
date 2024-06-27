23f2b1f475f307471a333503608597563b9f775c
Debasish Bhola <devop69@gmail.com>
2024-06-27 09:26:08 +0530

    ContextAPI

--- 7PropDrilling/src/App.jsx
+++ 7PropDrilling/src/App.jsx
@@ -1,5 +1,6 @@
 //Prop Drilling --->Getting props from Least Common Ancestor(Imagine as a binary tree)
 //Suppose there are 4 states c2,c2,c3 and c4; Suppose c4 needs prop which c1 has "Note that that prop doesn't need by c2 or c3" but they still have to pass it down to c4 
+
 import { useState } from "react";
 
 function App() {
@@ -7,13 +8,16 @@ function App() {
 
   return (
     <>
-      <Count count={count} />
-      <Button count={count} setCount={setCount} />
+      <Count count={count} setCount={setCount} />
+      
     </>
   );  
 }
-function Count({ count }) {
-  return <div>{count}</div>;
+function Count({ count ,setCount}) {
+  return <div>{count}
+  <Button count={count} setCount={setCount} /> 
+  </div>;
+  
 }
 
 function Button({count,setCount}) {
