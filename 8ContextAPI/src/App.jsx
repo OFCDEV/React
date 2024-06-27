1e6b68c366dec5446b8923f9cf5d92f359a1a8cf
Debasish Bhola <devop69@gmail.com>
2024-06-27 09:55:15 +0530

    Completed

--- 8ContextAPI/src/App.jsx
+++ 8ContextAPI/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Passing props can become verbose and inconvenient when you need to pass the props deeply through the tree, To overcome from this we use Context API
+
+import { useContext, useState } from "react";
+import { CountContext } from "./context";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  //Wrap anyone that wants to use the teleported value inside a provider(A provider is that something which provides the context value later on)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CountContext.Provider value={count}>
+        <Count count={count} setCount={setCount} />
+      </CountContext.Provider>
+      
     </>
-  )
+  );  
+}
+function Count({setCount}) {
+  return <div>
+  <CountRender/>
+  <Button setCount={setCount} /> 
+  </div>;
+  
+}
+function CountRender(){
+  const count = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
 }
 
-export default App
+function Button({setCount}) {
+  const count = useContext(CountContext)
+  return (
+    <div>
+      <button onClick={() => {setCount(count+1)}}>Increase</button>
+
+      <button onClick={() => {setCount(count-1)}}>Decrease</button>
+    </div>
+  );
+}
+
+export default App;
