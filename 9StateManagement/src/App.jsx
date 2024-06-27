fe3221cf4a603fdfd5a685b6f07777138d4d02c1
Debasish Bhola <devop69@gmail.com>
2024-06-27 18:38:24 +0530

    npm i recoil

--- 9StateManagement/src/App.jsx
+++ 9StateManagement/src/App.jsx
@@ -1,4 +1,11 @@
+//ContextAPI only helps to get rid of prop drilling, it doesn't solve the re-rendering of that state.
 
+//What is State Management?
+//A cleaner way to store the state of your app,also solves the problem of re-rendering in Context API
+
+//State Management using Recoil
+//Recoil-->A state management library for React; Recoil has a concept of an atom to store the state
+//An atom can be defined outside the component
 
 function App() {
 
