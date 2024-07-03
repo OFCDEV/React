349f5c847bdd9b0de45ffadb0753fd40c62ec26e
Debasish Bhola <devop69@gmail.com>
2024-07-03 17:00:09 +0530

    Completed

--- 9StateManagement/src/App.jsx
+++ 9StateManagement/src/App.jsx
@@ -1,7 +1,8 @@
 //ContextAPI only helps to get rid of prop drilling, it doesn't solve the re-rendering of that state.
 
 import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
-import { countAtom } from "../store/atoms/count";
+import { countAtom, evenSelector } from "../store/atoms/count";
+import { useMemo } from "react";
 
 //What is State Management?
 //A cleaner way to store the state of your app,also solves the problem of re-rendering in Context API
@@ -33,6 +34,18 @@ function CountRender(){
   const count = useRecoilValue(countAtom);
   return <div>
     {count}
+    <EvenCountRendered/>
+  </div>
+}
+
+function EvenCountRendered(){
+  const count = useRecoilValue(countAtom);
+  //Optimization-->We can use useMemo or Selector from recoil
+  // const isEven = useMemo(()=>{return count%2 == 0;},[count]);
+
+  const isEven = useRecoilValue(evenSelector);
+  return <div>
+    {isEven ? "It is odd" : null}
   </div>
 }
 
--- 9StateManagement/store/atoms/count.jsx
+++ 9StateManagement/store/atoms/count.jsx
@@ -6,9 +6,26 @@
 //2.useRecoilValue-->Just give the value
 //3.useSetRecoilValue-->Set value
 
-import {atom} from "recoil"
+import {atom, selector} from "recoil"
 
 export const countAtom = atom({
     key: "countAtom",
     default:0
 })
+
+export const evenSelector = selector({
+    key:"evenSelector",
+    get:({get})=>{
+        const count =get(countAtom);
+        return count%2
+    }
+});
+
+
+// export const oddSelector = selector({
+//     name:'oddSelector',
+//     get:(props)=>{
+//         const count =props.get(countAtom);
+//         return !count%2
+//     }
+// });
